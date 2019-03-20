e0bed8ff9726023774062dcc939ea83425950665
Darshan Markandaiah <darshan.markandaiah@gmail.com>
2019-03-20 13:28:10 -0400

    Update src.py

--- plugin/src.py
+++ plugin/src.py
@@ -14,12 +14,12 @@ RUN_TEST_CMD = "{test_runner}  {path}"
 
 TEST_RUNNER = vim.eval('g:test_runner')
 
-if vim.eval("exists('g:setup_cmd')"):
+if int(vim.eval("exists('g:setup_cmd')")):
     SETUP_CMD = vim.eval('g:setup_cmd')
 else:
     SETUP_CMD = None
 
-if vim.eval("exists('g:setup_test_runner_cmd')"):
+if int(vim.eval("exists('g:setup_test_runner_cmd')")):
     SETUP_TEST_RUNNER_CMD = vim.eval('g:setup_test_runner_cmd')
 else:
     SETUP_TEST_RUNNER_CMD = None
